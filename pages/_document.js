48b0b3500917a9801eb10eb96be76ec9214a76f1
Cleison Carlos <cleisonspider@yahoo.com.br>
2023-07-18 16:51:07 -0300

    fist commit

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
-      <body>
+      <body className=' text-zinc-50 bg-gradient-to-b from-green-500 to-zinc-900 bg-zinc-900'>
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,232 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+import { House, MagnifyingGlass, Books, ArrowSquareLeft, ArrowSquareRight, PlayCircle, SkipBack, SkipForward, Shuffle, Repeat } from "@phosphor-icons/react";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='h-screen flex flex-col'>
+
+      <div className='flex flex-1'>
+        <aside className='w-64 bg-zinc-950 p-6'>
+
+
+          <nav className='space-y-2'>
+            <a href='#' className='flex items-center gap-2 font-semibold'>
+              <House size={24} weight="duotone" />
+              Home
+            </a>
+
+
+            <a href='#' className='flex items-center gap-2 font-semibold'>
+              <MagnifyingGlass size={24} weight="duotone" />
+              Search
+            </a>
+
+            <a href='#' className='flex items-center gap-2 font-semibold'>
+              <Books size={24} weight="duotone" />
+              Library
+            </a>
+
+
+
+          </nav>
+
+
+
+        </aside>
+        <main className='p-6 bg-gradient-to-b from-green-500 to-zinc-900'>
+
+          <div className='flex items-center gap-3'>
+
+            <button>
+              <ArrowSquareLeft size={32} weight="duotone" />
+            </button>
+
+            <button>
+              <ArrowSquareRight size={32} weight="duotone" />
+            </button>
+
+          </div>
+
+          <h1 className='font-bold text-3xl my-5'>Good Morning</h1>
+
+
+
+          <div className='grid grid-cols-3 gap-4'>
+
+            <div className='bg-white/10 rounded overflow-hidden flex items-center gap-4 group hover:bg-black/10'>
+
+              <Image width={160} height={160} src={'/beatles-cover.jpg'} alt='capa do disco' />
+              <strong>Nome do Álbum</strong>
+
+              <button className='mr-4 invisible group-hover:visible'>
+                <PlayCircle size={60} color="#22c25d" weight="duotone" />
+              </button>
+            </div>
+
+
+            <div className='bg-white/10 rounded overflow-hidden flex items-center gap-4 group hover:bg-black/10'>
+
+              <Image width={160} height={160} src={'/beatles-cover.jpg'} alt='capa do disco' />
+              <strong>Nome do Álbum</strong>
+
+              <button className='mr-4 invisible group-hover:visible'>
+                <PlayCircle size={60} color="#22c25d" weight="duotone" />
+              </button>
+            </div>
+
+
+
+            <div className='bg-white/10 rounded overflow-hidden flex items-center gap-4 group hover:bg-black/10'>
+
+              <Image width={160} height={160} src={'/beatles-cover.jpg'} alt='capa do disco' />
+              <strong>Nome do Álbum</strong>
+
+              <button className='mr-4 invisible group-hover:visible'>
+                <PlayCircle size={60} color="#22c25d" weight="duotone" />
+              </button>
+            </div>
+
+
+
+            <div className='bg-white/10 rounded overflow-hidden flex items-center gap-4 group hover:bg-black/10'>
+
+              <Image width={160} height={160} src={'/beatles-cover.jpg'} alt='capa do disco' />
+              <strong>Nome do Álbum</strong>
+
+              <button className='mr-4 invisible group-hover:visible'>
+                <PlayCircle size={60} color="#22c25d" weight="duotone" />
+              </button>
+            </div>
+
+
+
+            <div className='bg-white/10 rounded overflow-hidden flex items-center gap-4 group hover:bg-black/10'>
+
+              <Image width={160} height={160} src={'/beatles-cover.jpg'} alt='capa do disco' />
+              <strong>Nome do Álbum</strong>
+
+              <button className='mr-4 invisible group-hover:visible'>
+                <PlayCircle size={60} color="#22c25d" weight="duotone" />
+              </button>
+            </div>
+
+
+
+
+          </div>
+
+
+
+          <h1 className='font-bold text-2xl my-10'>Made for Cleison Carlos</h1>
+
+          <div className='grid grid-cols-6 gap-6'>
+
+            <div className='bg-white/10 overflow-hidden rounded hover:bg-black/10 mb-5'>
+
+              <Image width={240} height={240} src={'/beatles-cover.jpg'} alt='capa do disco' className='w-full p-2' />
+              <div className='p-2'>
+                <strong>Nome do Álbum</strong>
+                <p className='text-zinc-400'>Lorem Ipsum Dolor</p>
+
+              </div>
+            </div>
+
+            <div className='bg-white/10 overflow-hidden rounded hover:bg-black/10 mb-5'>
+
+              <Image width={240} height={240} src={'/beatles-cover.jpg'} alt='capa do disco' className='w-full p-2' />
+              <div className='p-2'>
+                <strong>Nome do Álbum</strong>
+                <p className='text-zinc-400'>Lorem Ipsum Dolor</p>
+
+              </div>
+            </div>
+
+            <div className='bg-white/10 overflow-hidden rounded hover:bg-black/10 mb-5'>
+
+              <Image width={240} height={240} src={'/beatles-cover.jpg'} alt='capa do disco' className='w-full p-2' />
+              <div className='p-2'>
+                <strong>Nome do Álbum</strong>
+                <p className='text-zinc-400'>Lorem Ipsum Dolor</p>
+
+              </div>
+            </div>
+
+            <div className='bg-white/10 overflow-hidden rounded hover:bg-black/10 mb-5'>
+
+              <Image width={240} height={240} src={'/beatles-cover.jpg'} alt='capa do disco' className='w-full p-2' />
+              <div className='p-2'>
+                <strong>Nome do Álbum</strong>
+                <p className='text-zinc-400'>Lorem Ipsum Dolor</p>
+
+              </div>
+            </div>
+
+            <div className='bg-white/10 overflow-hidden rounded hover:bg-black/10 mb-5'>
+
+              <Image width={240} height={240} src={'/beatles-cover.jpg'} alt='capa do disco' className='w-full p-2' />
+              <div className='p-2'>
+                <strong>Nome do Álbum</strong>
+                <p className='text-zinc-400'>Lorem Ipsum Dolor</p>
+
+              </div>
+            </div>
+
+            <div className='bg-white/10 overflow-hidden rounded hover:bg-black/10 mb-5'>
+              <Image width={240} height={240} src={'/beatles-cover.jpg'} alt='capa do disco' className='w-full p-2' />
+              <div className='p-2'>
+                <strong>Nome do Álbum</strong>
+                <p className='text-zinc-400'>Lorem Ipsum Dolor</p>
+
+              </div>
+            </div>
+
+          </div>
+
+        </main>
+      </div>
+
+      <footer className='bg-zinc-900 p-6 border-t border-zinc-700 flex justify-between'>
+
+        <div className='flex items-center gap-2'>
+          <Image width={60} height={60} src={'/beatles-cover.jpg'} alt='capa do disco' />
+          <div className='flex flex-col gap-1'>
+            <strong>Nome da Música</strong>
+            <span className='text-zinc-400'>Nome do artista</span>
+          </div>
+
         </div>
-      </div>
+        <div className='flex flex-col items-center'>
+          <div className='flex gap-3'>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <button>
+              <Shuffle size={32} weight="duotone" />
+            </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <button>
+              <SkipBack size={40} weight="duotone" />
+            </button>
+            <button>
+              <PlayCircle size={46} weight="duotone" />
+            </button>
+            <button>
+              <SkipForward size={40} weight="duotone" />
+            </button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+            <button>
+              <Repeat size={32} weight="duotone" />
+            </button>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </div>
+          
+        </div>
+
+      </footer>
+
+    </div>
   )
 }
